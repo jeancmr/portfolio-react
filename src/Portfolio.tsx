f4ee4ecfa7566809cb9096aca6efa5fe3bafb6ab
jeancmr <jean.madiedo08@gmail.com>
2025-12-16 09:57:33 -0500

    feat: add tailwind

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,3 +1,24 @@
 export const Portfolio = () => {
-  return <h1>Portfolio</h1>;
+  return (
+    <>
+      <header className="grid grid-cols-2">
+        <div className="info">
+          <h1>Fullstack developer</h1>
+          <p>I'm a passionate developer with experience in building web applications.</p>
+
+          <div>
+            <button>View My Work</button>
+            <button>Contact Me</button>
+          </div>
+        </div>
+        <div className="photo">
+          <img
+            src="/profilepic.webp"
+            alt="Developer Photo"
+            className="h-52 w-52 rounded-full object-cover"
+          />
+        </div>
+      </header>
+    </>
+  );
 };
